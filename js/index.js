263de48563cb3f1074e66503a7678e8686f27060
Santiago Garcia <sangarz883@gmail.com>
2022-11-20 17:27:50 -0300

    0.0

--- js/index.js
+++ js/index.js
@@ -1,5 +1,15 @@
-    var button = document.querySelectorAll(".tembut");
+    var b1 = document.querySelector('#i1');
+    var plato = document.querySelector('#plato1')
 
-    button.addEventListener("click", function() {
-        console.log("deu certo")
-    })
+   
+  b1.addEventListener('click', function(plato){
+        var p1 = document.querySelector('#p1');
+        var h1 = document.querySelector('#h1');
+        if (document.querySelector('#p1').style.display == 'block'){
+            p1.style.display = 'none';
+            h1.style='margin.top', '20pxpx';
+            }else{
+            p1.style.display = 'block';
+            h1.style.margin = '15px';
+            }
+  });
