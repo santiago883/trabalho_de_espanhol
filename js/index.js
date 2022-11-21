526b2c8bae28e37e5eb86152b7d4105cef221510
Santiago Garcia <sangarz883@gmail.com>
2022-11-21 00:53:50 -0300

    1.0

--- js/index.js
+++ js/index.js
@@ -1,8 +1,18 @@
     var b1 = document.querySelector('#i1');
-    var plato = document.querySelector('#plato1')
+    var b2 = document.querySelector('#i2');
+    var b3 = document.querySelector('#i3');
+    var b4 = document.querySelector('#i4');
+    var b5 = document.querySelector('#i5');
+    var b6 = document.querySelector('#i6');
+    var b7 = document.querySelector('#i7');
+    var b8 = document.querySelector('#i8');
+    var b9 = document.querySelector('#i9');
+    var b10 = document.querySelector('#i10');
+    var b11 = document.querySelector('#i11');
+    var b12 = document.querySelector('#i12');
 
    
-  b1.addEventListener('click', function(plato){
+  b1.addEventListener('click', function(){
         var p1 = document.querySelector('#p1');
         var h1 = document.querySelector('#h1');
         if (document.querySelector('#p1').style.display == 'block'){
@@ -13,3 +23,140 @@
             h1.style.margin = '15px';
             }
   });
+
+
+
+    b2.addEventListener('click', function(){
+    var p2 = document.querySelector('#p2');
+    var h2 = document.querySelector('#h2');
+    if (document.querySelector('#p2').style.display == 'block'){
+        p2.style.display = 'none';
+        h2.style='margin.top', '20pxpx';
+        }else{
+        p2.style.display = 'block';
+        h2.style.margin = '15px';
+        }
+    });
+
+    b3.addEventListener('click', function(){
+    var p3 = document.querySelector('#p3');
+    var h3 = document.querySelector('#h3');
+    if (document.querySelector('#p3').style.display == 'block'){
+        p3.style.display = 'none';
+        h3.style='margin.top', '20pxpx';
+        }else{
+        p3.style.display = 'block';
+        h3.style.margin = '15px';
+        }
+    });
+
+    b4.addEventListener('click', function(){
+        var p4 = document.querySelector('#p4');
+        var h4 = document.querySelector('#h4');
+        if (document.querySelector('#p4').style.display == 'block'){
+            p4.style.display = 'none';
+            h4.style='margin.top', '20pxpx';
+            }else{
+            p4.style.display = 'block';
+            h4.style.margin = '15px';
+            }
+    });
+
+    b5.addEventListener('click', function(){
+        var p5 = document.querySelector('#p5');
+        var h5 = document.querySelector('#h5');
+        if (document.querySelector('#p5').style.display == 'block'){
+            p5.style.display = 'none';
+            h5.style='margin.top', '20pxpx';
+            }else{
+            p5.style.display = 'block';
+            h5.style.margin = '15px';
+            }
+    });
+
+    b6.addEventListener('click', function(){
+        var p6 = document.querySelector('#p6');
+        var h6 = document.querySelector('#h6');
+        if (document.querySelector('#p6').style.display == 'block'){
+            p6.style.display = 'none';
+            h6.style='margin.top', '20pxpx';
+            }else{
+            p6.style.display = 'block';
+            h6.style.margin = '15px';
+            }
+    });
+
+    b7.addEventListener('click', function(){
+        var p7 = document.querySelector('#p7');
+        var h7 = document.querySelector('#h7');
+        if (document.querySelector('#p7').style.display == 'block'){
+            p7.style.display = 'none';
+            h7.style='margin.top', '20pxpx';
+            }else{
+            p7.style.display = 'block';
+            h7.style.margin = '15px';
+            }
+    });
+
+    b8.addEventListener('click', function(){
+        var p8 = document.querySelector('#p8');
+        var h8 = document.querySelector('#h8');
+        if (document.querySelector('#p8').style.display == 'block'){
+            p8.style.display = 'none';
+            h8.style='margin.top', '20pxpx';
+            }else{
+            p8.style.display = 'block';
+            h8.style.margin = '15px';
+            }
+    });
+
+    b9.addEventListener('click', function(){
+        var p9 = document.querySelector('#p9');
+        var h9 = document.querySelector('#h9');
+        if (document.querySelector('#p9').style.display == 'block'){
+            p9.style.display = 'none';
+            h9.style='margin.top', '20pxpx';
+            }else{
+            p9.style.display = 'block';
+            h9.style.margin = '15px';
+            }
+    });
+
+    b10.addEventListener('click', function(){
+        var p10 = document.querySelector('#p10');
+        var h10 = document.querySelector('#h10');
+        if (document.querySelector('#p10').style.display == 'block'){
+            p10.style.display = 'none';
+            h10.style='margin.top', '20pxpx';
+            }else{
+            p10.style.display = 'block';
+            h10.style.margin = '15px';
+            }
+    });
+
+    b11.addEventListener('click', function(){
+        var p11 = document.querySelector('#p11');
+        var h11 = document.querySelector('#h11');
+        if (document.querySelector('#p11').style.display == 'block'){
+            p11.style.display = 'none';
+            h11.style='margin.top', '20pxpx';
+            }else{
+            p11.style.display = 'block';
+            h11.style.margin = '15px';
+            }
+    });
+
+    b12.addEventListener('click', function(){
+        var p12 = document.querySelector('#p12');
+        var h12 = document.querySelector('#h12');
+        if (document.querySelector('#p12').style.display == 'block'){
+            p12.style.display = 'none';
+            h12.style='margin.top', '20pxpx';
+            }else{
+            p12.style.display = 'block';
+            h12.style.margin = '15px';
+            }
+    });
+
+
+
