3f562b788dd51fa59cb04c3d82168f940fb63d6d
Santiago Garcia <sangarz883@gmail.com>
2022-11-21 22:55:50 -0300

    1.5

--- js/index.js
+++ js/index.js
@@ -10,7 +10,6 @@
     var b10 = document.querySelector('#i10');
     var b11 = document.querySelector('#i11');
     var b12 = document.querySelector('#i12');
-
    
   b1.addEventListener('click', function(){
         var p1 = document.querySelector('#p1');
@@ -160,3 +159,257 @@
 
 
 
+
+    var img1 = document.querySelector('#img1');
+    var img2 = document.querySelector('#img2');
+    var img3 = document.querySelector('#img3');
+    var img4 = document.querySelector('#img4');
+    var img5 = document.querySelector('#img5');
+    var img6 = document.querySelector('#img6');
+    var img7 = document.querySelector('#img7');
+    var img8 = document.querySelector('#img8');
+    var img9 = document.querySelector('#img9');
+    var img10 = document.querySelector('#img10');
+    var img11 = document.querySelector('#img11');
+    var img12 = document.querySelector('#img12');
+
+    img1.addEventListener('click', function(){
+        var im1 = document.querySelector('#im1')
+        if(document.querySelector('#im1').display == 'block'){
+            im1.style.display = 'none';
+        console.log('ola')
+        }else{
+            im1.style.display = 'block';
+        }
+    })
+
+    img2.addEventListener('click', function(){
+        var im2 = document.querySelector('#im2')
+        if(document.querySelector('#im2').display == 'block'){
+            im2.style.display = 'none';
+        console.log('ola')
+        }else{
+            im2.style.display = 'block';
+        }
+    })
+    
+    img3.addEventListener('click', function(){
+        var im3 = document.querySelector('#im3')
+        if(document.querySelector('#im3').display == 'block'){
+            im3.style.display = 'none';
+        console.log('ola')
+        }else{
+            im3.style.display = 'block';
+        }
+    })
+    
+    img4.addEventListener('click', function(){
+        var im4 = document.querySelector('#im4')
+        if(document.querySelector('#im4').display == 'block'){
+            im1.style.display = 'none';
+        console.log('ola')
+        }else{
+            im4.style.display = 'block';
+        }
+    })
+    
+    img5.addEventListener('click', function(){
+        var im5 = document.querySelector('#im5')
+        if(document.querySelector('#im5').display == 'block'){
+            im5.style.display = 'none';
+        console.log('ola')
+        }else{
+            im5.style.display = 'block';
+        }
+    })
+    
+    img6.addEventListener('click', function(){
+        var im6 = document.querySelector('#im6')
+        if(document.querySelector('#im6').display == 'block'){
+            im6.style.display = 'none';
+        console.log('ola')
+        }else{
+            im6.style.display = 'block';
+        }
+    })
+    
+    img7.addEventListener('click', function(){
+        var im7 = document.querySelector('#im7')
+        if(document.querySelector('#im7').display == 'block'){
+            im7.style.display = 'none';
+        console.log('ola')
+        }else{
+            im7.style.display = 'block';
+        }
+    })
+    
+    img8.addEventListener('click', function(){
+        var im8 = document.querySelector('#im8')
+        if(document.querySelector('#im8').display == 'block'){
+            im8.style.display = 'none';
+        console.log('ola')
+        }else{
+            im8.style.display = 'block';
+        }
+    })
+    
+    img9.addEventListener('click', function(){
+        var im9 = document.querySelector('#im9')
+        if(document.querySelector('#im9').display == 'block'){
+            im9.style.display = 'none';
+        console.log('ola')
+        }else{
+            im9.style.display = 'block';
+        }
+    })
+
+    img10.addEventListener('click', function(){
+        var im10 = document.querySelector('#im10')
+        if(document.querySelector('#im10').display == 'block'){
+            im10.style.display = 'none';
+        console.log('ola')
+        }else{
+            im10.style.display = 'block';
+        }
+    })
+    
+    img11.addEventListener('click', function(){
+        var im11 = document.querySelector('#im11')
+        if(document.querySelector('#im11').display == 'block'){
+            im11.style.display = 'none';
+        console.log('ola')
+        }else{
+            im11.style.display = 'block';
+        }
+    })
+    
+    img12.addEventListener('click', function(){
+        var im12 = document.querySelector('#im12')
+        if(document.querySelector('#im12').display == 'block'){
+            im12.style.display = 'none';
+        console.log('ola')
+        }else{
+            im12.style.display = 'block';
+        }
+    })
+
+    var im1 = document.querySelector('#im1');
+    var im2 = document.querySelector('#im2');
+    var im3 = document.querySelector('#im3');
+    var im4 = document.querySelector('#im4');
+    var im5 = document.querySelector('#im5');
+    var im6 = document.querySelector('#im6');
+    var im7 = document.querySelector('#im7');
+    var im8 = document.querySelector('#im8');
+    var im9 = document.querySelector('#im9');
+    var im10 = document.querySelector('#im10');
+    var im11 = document.querySelector('#im11');
+    var im12 = document.querySelector('#im12');
+    
+    im1.addEventListener('click', function(){
+        if(document.querySelector('#im1').display == 'none'){
+            im1.style.display = 'block';
+        console.log('ola')
+        }else{
+            im1.style.display = 'none';
+        }
+    })
+
+    im2.addEventListener('click', function(){
+        if(document.querySelector('#im2').display == 'none'){
+            im2.style.display = 'block';
+        console.log('ola')
+        }else{
+            im2.style.display = 'none';
+        }
+    })
+    
+    im3.addEventListener('click', function(){
+        if(document.querySelector('#im3').display == 'none'){
+            im3.style.display = 'block';
+        console.log('ola')
+        }else{
+            im3.style.display = 'none';
+        }
+    })
+    
+    im4.addEventListener('click', function(){
+        if(document.querySelector('#im4').display == 'none'){
+            im1.style.display = 'block';
+        console.log('ola')
+        }else{
+            im4.style.display = 'none';
+        }
+    })
+    
+    im5.addEventListener('click', function(){
+        if(document.querySelector('#im5').display == 'none'){
+            im5.style.display = 'block';
+        console.log('ola')
+        }else{
+            im5.style.display = 'none';
+        }
+    })
+    
+    im6.addEventListener('click', function(){
+        if(document.querySelector('#im6').display == 'none'){
+            im6.style.display = 'block';
+        console.log('ola')
+        }else{
+            im6.style.display = 'none';
+        }
+    })
+    
+    im7.addEventListener('click', function(){
+        if(document.querySelector('#im7').display == 'none'){
+            im7.style.display = 'block';
+        console.log('ola')
+        }else{
+            im7.style.display = 'none';
+        }
+    })
+    
+    im8.addEventListener('click', function(){
+        if(document.querySelector('#im8').display == 'none'){
+            im8.style.display = 'block';
+        console.log('ola')
+        }else{
+            im8.style.display = 'none';
+        }
+    })
+    
+    im9.addEventListener('click', function(){
+        if(document.querySelector('#im9').display == 'none'){
+            im9.style.display = 'block';
+        console.log('ola')
+        }else{
+            im9.style.display = 'none';
+        }
+    })
+
+    im10.addEventListener('click', function(){
+        if(document.querySelector('#im10').display == 'none'){
+            im10.style.display = 'block';
+        console.log('ola')
+        }else{
+            im10.style.display = 'none';
+        }
+    })
+    
+    im11.addEventListener('click', function(){
+        if(document.querySelector('#im11').display == 'none'){
+            im11.style.display = 'block';
+        console.log('ola')
+        }else{
+            im11.style.display = 'none';
+        }
+    })
+    
+    im12.addEventListener('click', function(){
+        if(document.querySelector('#im12').display == 'none'){
+            im12.style.display = 'block';
+        console.log('ola')
+        }else{
+            im12.style.display = 'none';
+        }
+    })
